312e80048593898065c563671e3ba20800f7fd20
Nathan Lesne <nathan.lesne@etu.u-bordeaux.fr>
2019-04-04 12:24:43 +0200

    Added script to download required project librairies

---
+++ lib/get_libs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+JFLEX_ARCHIVE="jflex-1.7.0.tar.gz"
+JFLEX_DIR="jflex-1.7.0"
+BEAVER_ARCHIVE="beaver-0.9.11.zip"
+BEAVER_DIR="beaver-0.9.11"
+
+echo -e "https://www.jflex.de/release/$JFLEX_ARCHIVE\nhttps://freefr.dl.sourceforge.net/project/beaver/$BEAVER_ARCHIVE" > libs.txt
+wget -i libs.txt && rm libs.txt
+tar -xzf $JFLEX_ARCHIVE && rm $JFLEX_ARCHIVE
+unzip $BEAVER_ARCHIVE && rm $BEAVER_ARCHIVE
+mv "$JFLEX_DIR/lib/jflex-full-1.7.0.jar" .
+rm -rf $JFLEX_DIR
+mv -v $BEAVER_DIR/lib/* .
+
+
