d69e37de3bc827014962b8ae4ed96eb3df10b52e
Toastation <toastations@gmail.com>
2019-04-22 22:27:03 +0200

    stackEnvironment works (no destack though)

--- src/ubordeaux/deptinfo/compilation/project/environment/StackEnvironment.java
+++ src/ubordeaux/deptinfo/compilation/project/environment/StackEnvironment.java
@@ -1,16 +1,50 @@
 package ubordeaux.deptinfo.compilation.project.environment;
 
+import java.util.Iterator;
 import java.util.Stack;
+import java.util.HashMap;
 import ubordeaux.deptinfo.compilation.project.node.Node;
+import ubordeaux.deptinfo.compilation.project.type.Type;
 
 public class StackEnvironment extends Environment {
 
-  private Stack<Node> stack;
+  private Stack<HashMap<String, Type>> stack;
 
   public StackEnvironment(String label) {
     super(label);
-    stack = new Stack<Node>();
+    stack = new Stack<HashMap<String, Type>>();
   }
 
+  public void addToCurrentScope(String id, Type t) {
+    HashMap<String, Type> currentScope = stack.peek();
+    currentScope.put(id, t);
+  }
+
+  public void newScope() {
+    stack.push(new HashMap<String, Type>());
+  }
+
+  /**
+   * Retourne le type de la variable "id"
+   * Retourne null si la variable n'existe pas
+   */
+  public Type getType(String id) {
+      Type res = null;
+      Iterator it = stack.iterator(); // note : l'itérateur va du bas de la pile vers le haut
+      while (it.hasNext()) {
+        HashMap<String, Type> scope = (HashMap<String, Type>)it.next();
+        if (scope.containsKey(id))
+          res = scope.get(id);
+      }
+      return res;
+  }
+
+  /**
+   * Retourne vrai s'il existe une variable de nom "id" qui existe dans la portée actuelle
+   */
+  public boolean existsInScope(String id) {
+    HashMap<String, Type> currentScope = stack.peek();
+    return currentScope.containsKey(id);
+  }
 
 }
