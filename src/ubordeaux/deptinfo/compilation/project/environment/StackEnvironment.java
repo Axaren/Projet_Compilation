54969b95bb013ba2646cc7022fdfca59b6e91df8
Meven <melvin.even@etu.u-bordeaux.fr>
2019-04-26 13:39:25 +0200

    linear time stackEnvironment implementation

--- src/ubordeaux/deptinfo/compilation/project/environment/StackEnvironment.java
+++ src/ubordeaux/deptinfo/compilation/project/environment/StackEnvironment.java
@@ -1,33 +1,39 @@
 package ubordeaux.deptinfo.compilation.project.environment;
 
 import java.util.Iterator;
-import java.util.Stack;
+import java.util.ArrayList;
 import java.util.HashMap;
 import ubordeaux.deptinfo.compilation.project.node.Node;
 import ubordeaux.deptinfo.compilation.project.type.Type;
 
 public class StackEnvironment extends Environment {
 
-  private Stack<HashMap<String, Type>> stack;
+  private ArrayList<HashMap<String, Type>> stack;
 
   public StackEnvironment(String label) {
     super(label);
-    stack = new Stack<HashMap<String, Type>>();
+    stack = new ArrayList<HashMap<String, Type>>();
+  }
+
+  public HashMap<String, Type> peek() {
+    if (stack.size() == 0) return null;
+    return stack.get(stack.size() - 1);
   }
 
   public void addToCurrentScope(String id, Type t) {
-    HashMap<String, Type> currentScope = stack.peek();
-    currentScope.put(id, t);
+    HashMap<String, Type> currentScope = peek();
+    if (currentScope != null) currentScope.put(id, t);
   }
 
   public void newScope() {
-    stack.push(new HashMap<String, Type>());
+    stack.add(new HashMap<String, Type>());
+    System.out.println("added scope, size="+stack.size());
   }
 
   public void removeScope() {
-    System.out.println("stack size "+stack.size());
     if (stack.size() > 0)
-      stack.pop();
+      stack.remove(stack.size() - 1);
+    System.out.println("removed scope, size="+stack.size());
   }
 
   /**
@@ -35,21 +41,23 @@ public class StackEnvironment extends Environment {
    * Retourne null si la variable n'existe pas
    */
   public Type getType(String id) {
-      Type res = null;
-      Iterator it = stack.iterator(); // note : l'itérateur va du bas de la pile vers le haut
-      while (it.hasNext()) {
-        HashMap<String, Type> scope = (HashMap<String, Type>)it.next();
-        if (scope.containsKey(id))
-          res = scope.get(id);
+      if (stack.size() == 0) return null;
+      HashMap<String, Type> currentScope;
+      for (int i = stack.size() - 1; i >= 0; i--) {
+        currentScope = stack.get(i);
+        if (currentScope.containsKey(id)) {
+          return currentScope.get(id);
+        }
       }
-      return res;
+      return null;
   }
 
   /**
    * Retourne vrai s'il existe une variable de nom "id" qui existe dans la portée actuelle
    */
   public boolean existsInScope(String id) {
-    HashMap<String, Type> currentScope = stack.peek();
+    HashMap<String, Type> currentScope = peek();
+    if (currentScope == null) return false;
     return currentScope.containsKey(id);
   }
 
