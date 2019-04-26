85e026219c01fa8992fd064460447e7b80ececc0
Nathan Lesne <nathan.lesne@etu.u-bordeaux.fr>
2019-04-26 13:11:29 +0200

    Tweaked NodeOp with enum instead of String

--- src/ubordeaux/deptinfo/compilation/project/node/NodeOp.java
+++ src/ubordeaux/deptinfo/compilation/project/node/NodeOp.java
@@ -4,22 +4,46 @@ import ubordeaux.deptinfo.compilation.project.intermediateCode.Binop;
 
 public class NodeOp extends NodeExp {
 
-	protected String name;
+	private Operation operation;
+
+	public enum Operation {
+		PLUS(0),
+		MINUS(1),
+		MUL(2),
+		DIV(3),
+		AND(4),
+		OR(5),
+		LSHIFT(6),
+		RSHIFT(7),
+		ARSHIFT(8),
+		XOR(9),
+		NOT(-1);
+
+		public int getOp() {
+			return op;
+		}
+
+		private int op;
+
+		Operation(int op) {
+			this.op = op;
+		}
+	}
 
 	// Opération binaire
 	// f : E X F -> F
-	public NodeOp(String name, NodeExp op1, NodeExp op2) {
+	public NodeOp(Operation operation, NodeExp op1, NodeExp op2) {
 		super(op1, op2);
-		this.name = name;
+		this.operation = operation;
 		// le type d'un opérateur 
 		NodeExp exprFct = (NodeExp) this.get(1);
 		type = exprFct.type;
 	}
 
-	public NodeOp(String name, NodeExp op) {
+	public NodeOp(Operation operation, NodeExp op) {
 		super(op);
-		this.name = name;
-		// le type d'un opérateur 
+		this.operation = operation;
+		// le type d'un opérateur
 		NodeExp exprFct = (NodeExp) this.get(0);
 		type = exprFct.type;
 	}
@@ -27,9 +51,8 @@ public class NodeOp extends NodeExp {
 	@Override
 	public boolean checksType() {
 		super.checksType();
-		if ((super.size() > 1) && getOp2()!=null && !this.getOp1().getType().equals(this.getOp2().getType()))
-			return false;
-		return true;
+		return (super.size() <= 1) || getOp2() == null || this.getOp1().getType()
+				.equals(this.getOp2().getType());
 	}
 
 	private NodeExp getOp1() {
@@ -43,27 +66,18 @@ public class NodeOp extends NodeExp {
 	@Override
 	public NodeOp clone() {
 		if (this.size()==1)
-			return new NodeOp(name, (NodeExp) getOp1().clone());
+			return new NodeOp(operation, (NodeExp) getOp1().clone());
 		else if (this.size()==2)
-			return new NodeOp(name, (NodeExp) getOp1().clone(), (NodeExp) getOp2().clone());
+			return new NodeOp(operation, (NodeExp) getOp1().clone(), (NodeExp) getOp2().clone());
 		return null;
-		};
-
-	public final static int PLUS = 0, MINUS = 1, MUL = 2, DIV = 3, AND = 4, OR = 5, LSHIFT = 6, RSHIFT = 7, ARSHIFT = 8,
-			XOR = 9;
+		}
 
 	@Override
 	public void generateIntermediateCode() {
 		getOp1().generateIntermediateCode();
 		getOp2().generateIntermediateCode();
 
-
-		switch (name){
-			case "PLUS": super.setExp(new Binop(PLUS, getOp1().getExp(), getOp2().getExp())); break;
-			case "MINUS": super.setExp(new Binop(MINUS, getOp1().getExp(), getOp2().getExp())); break;
-			case "TIMES": super.setExp(new Binop(MUL, getOp1().getExp(), getOp2().getExp())); break;
-			case "DIV": super.setExp(new Binop(DIV, getOp1().getExp(), getOp2().getExp())); break;
-		}
+		setExp(new Binop(operation.getOp(), getOp1().getExp(), getOp2().getExp()));
 
 	}
 }
