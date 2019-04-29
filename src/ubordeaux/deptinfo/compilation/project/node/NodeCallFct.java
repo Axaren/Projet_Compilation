500a5c4181fa1e5f7e7c8cb239dbfa074cc6ca46
maelleto-do <maelleto.do@gmail.com>
2019-04-29 17:13:24 +0200

    Oubli d'appliquer generateIntCode sur chaque Noeud - CallFct

--- src/ubordeaux/deptinfo/compilation/project/node/NodeCallFct.java
+++ src/ubordeaux/deptinfo/compilation/project/node/NodeCallFct.java
@@ -93,6 +93,7 @@ public final class NodeCallFct extends NodeExp {
 
 		while(itArgs.hasNext()){
 			this.addArgsList(itArgs.next().getExpList().getHead());
+			itArgs.next().generateIntermediateCode();
 		}
 
 		Name name = new Name(new LabelLocation(this.name));
