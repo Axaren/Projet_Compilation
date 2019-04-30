bdf1327ba13417b146a85fd13110e61f011bc15a
Nathan Lesne <nathan.lesne@etu.u-bordeaux.fr>
2019-04-30 15:35:00 +0200

    Added types to Nodes in dot files

--- src/ubordeaux/deptinfo/compilation/project/node/NodeExp.java
+++ src/ubordeaux/deptinfo/compilation/project/node/NodeExp.java
@@ -4,6 +4,11 @@ import ubordeaux.deptinfo.compilation.project.type.Type;
 
 public abstract class NodeExp extends Node {
 
+	@Override
+	protected String toDotNodeName() {
+		return super.toDotNodeName() + type.toString();
+	}
+
 	protected Type type;
 
 	public NodeExp(Node... abstTrees) {
