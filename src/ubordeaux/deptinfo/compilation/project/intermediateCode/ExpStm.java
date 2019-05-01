86bf88dc8895c8bb0660157697a6c02698e0613c
Toastation <toastations@gmail.com>
2019-05-01 16:50:33 +0200

    ExpStm implementation

--- src/ubordeaux/deptinfo/compilation/project/intermediateCode/ExpStm.java
+++ src/ubordeaux/deptinfo/compilation/project/intermediateCode/ExpStm.java
@@ -2,4 +2,22 @@ package ubordeaux.deptinfo.compilation.project.intermediateCode;
 
 public class ExpStm extends Stm {
 
+    private Exp exp;
+
+    public ExpStm(Exp exp) {
+        super();
+        this.exp = exp;
+    }
+
+    public Exp getExp() {
+        return exp;
+    } 
+
+    @Override
+    public String toString() {
+        return "ExpStm(" +
+                exp.toString() +
+                ")";
+    }
+
 }
